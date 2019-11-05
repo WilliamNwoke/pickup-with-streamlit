dec845f3d601561c3538d2caeafa5e001ae42e8d
William Nwoke <willteey@gmail.com>
2019-11-05 16:08:29 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[server[\n\
+[server]\n\
 headless = true\n\
 port = $PORT\n\
-enableCors = false\n\
+enableCORS = false\n\
 \n\
-" > ~/.streamlit/config.toml 
+" > ~/.streamlit/config.toml
